40f8423810201e8d2833f5f57f55e879e457baa8
CodeDraken <codedraken@users.noreply.github.com>
2018-10-16 00:00:42 -0500

    Add Window class

---
+++ Window.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const { BrowserWindow } = require('electron')
+
+// default window settings
+const defaultProps = {
+  width: 500,
+  height: 800,
+  show: false
+}
+
+class Window extends BrowserWindow {
+  constructor ({ file, ...windowSettings }) {
+    // calls new BrowserWindow with these props
+    super({ ...defaultProps, ...windowSettings })
+
+    // load the html and open devtools
+    this.loadFile(file)
+    this.webContents.openDevTools()
+
+    // gracefully show when ready to prevent flickering
+    this.once('ready-to-show', () => {
+      this.show()
+    })
+  }
+}
+
+module.exports = Window
