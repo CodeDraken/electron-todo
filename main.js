33a7140e161e4122dfaca5b5026794971608fe9c
CodeDraken <codedraken@users.noreply.github.com>
2018-10-16 23:13:15 -0500

    Add design

--- main.js
+++ main.js
@@ -4,9 +4,19 @@ const { app } = require('electron')
 
 const Window = require('./Window')
 
+require('electron-reload')(__dirname)
+
 function main () {
   let mainWindow = new Window({
     file: 'index.html'
+  })
+
+  let addTodoWin = new Window({
+    file: 'add.html',
+    width: 400,
+    height: 400,
+    // close with the main window
+    parent: mainWindow
   })
 }
 
