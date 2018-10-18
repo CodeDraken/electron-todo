50875b4b941d7ee8d739f41393fe3b8f09fdeec0
CodeDraken <codedraken@users.noreply.github.com>
2018-10-17 19:41:51 -0500

    Add DataStore

---
+++ DataStore.js
@@ -0,0 +1,41 @@
+
+const Store = require('electron-store')
+
+class DataStore extends Store {
+  constructor (settings) {
+    super(settings)
+
+    // initialize with todos or empty array
+    this.todos = this.get('todos') || []
+  }
+
+  saveTodos () {
+    // save todos to JSON file
+    this.set('todos', this.todos)
+
+    return this
+  }
+
+  getTodos () {
+    // set object's todos to todos in JSON file
+    this.todos = this.get('todos') || []
+
+    return this
+  }
+
+  addTodo (todo) {
+    // merge the existing todos with the new todo
+    this.todos = [ ...this.todos, todo ]
+
+    return this.saveTodos()
+  }
+
+  deleteTodo (todo) {
+    // filter out the target todo
+    this.todos = this.todos.filter(t => t !== todo)
+
+    return this.saveTodos()
+  }
+}
+
+module.exports = DataStore
--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 'use strict'
 
+const path = require('path')
 const { app } = require('electron')
 
 const Window = require('./Window')
+const DataStore = require('./DataStore')
 
 require('electron-reload')(__dirname)
 
+const todosData = new DataStore({ name: 'Todos Main' })
+
 function main () {
   let mainWindow = new Window({
-    file: 'index.html'
+    file: path.join('renderer', 'index.html')
   })
 
-  let addTodoWin = new Window({
-    file: 'add.html',
-    width: 400,
-    height: 400,
-    // close with the main window
-    parent: mainWindow
-  })
+  todosData
+    .addTodo('create todo app')
+    .addTodo('another todo')
+    .addTodo('one more todo')
+    .deleteTodo('another todo')
+
+  console.log(todosData.todos)
+  // [ 'create todo app', 'one more todo' ]
+
+  // let addTodoWin = new Window({
+  //   file: 'add.html',
+  //   width: 400,
+  //   height: 400,
+  //   // close with the main window
+  //   parent: mainWindow
+  // })
 }
 
 app.on('ready', main)
